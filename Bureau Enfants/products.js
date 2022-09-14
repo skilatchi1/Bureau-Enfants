c22e762dbf28a5ea218203118ff6f5f73c005b59
skilatchi1 <112979796+skilatchi1@users.noreply.github.com>
2022-09-14 18:52:19 +0200

    Add files via upload

---
+++ Bureau Enfants/products.js
@@ -0,0 +1,18 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.png',
+		image3: 'img/products/3.png',
+		image4: 'img/products/4.png',
+		image5: 'img/products/5.png',
+		title: 'السبورة السحرية للأطفال الصغار مع مكتب ومقعد Ferdi Plast Bureau Enfants Table Magic et Chaise',
+		price1: 5300,
+		price: 4700,
+		description: ' هل تبحث عن وسيلة تساعد أطفالك على التعلم والترفيه في نفس الوقت ؟ نعرض عليك السبورة السحرية 2 في 1 الخاصة بالروضات والمنازل تتميز بالمتانة والصلابة يمكن استعمالها كسبورة بيضاء للكتابة والرسم كما يمكن تحويلها إلى مكتب صغير لحل الواجبات مرفقة بأقلام ملونة وحروف لاصقة في السبورة و كرسي بلاستيكي. أغتنم الفرصة و أطلب الان.  '   ,
+		phone: 0555626156,
+	},
+
+	
+];  
